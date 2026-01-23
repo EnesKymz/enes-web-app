bbcd8780e699bd1480d679507fb75a306181132b
EnesKymz <okkeseneskaymaz@proton.me>
2026-01-23 15:21:41 +0300

    fix

--- app/components/profileCard.tsx
+++ app/components/profileCard.tsx
@@ -10,7 +10,7 @@ export default function ProfileCard(){
                 <div className="pl-4 md:pl-8 border-l-2 border-gray-700 ml-2 mt-2 mb-2 space-y-4">
                   <div className="bg-[#2d2d2d]/50 p-6 rounded-lg border border-gray-700 hover:border-blue-500 transition-all shadow-xl group cursor-default">
                     <div className="flex items-center gap-4 mb-4">
-                        <span className="text-blue-300">myImage:</span>
+                        <span className="text-blue-300">profilResim:</span>
                         <div className="relative group-hover:scale-105 transition-transform duration-300">
                             <span className="text-purple-400 absolute -left-4 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity">{"("}</span>
                             <Image
